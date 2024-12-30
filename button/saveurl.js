4ab2cf1e333553ffd3e643234a6aacd6bc553e13
DanceItBreakIt <danceitbreakit@gmail.com>
2024-12-29 19:00:30 -0500

    stuff added

---
+++ button/saveurl.js
@@ -0,0 +1,2 @@
+let userInput = document.getElementById("save").value;
+localStorage.setItem("userInput", userInput);
--- imagereplacer.js
+++ imagereplacer.js
@@ -1,10 +1,11 @@
-const images = document.images;
-const imageCount = document.images.length;
+let images = document.images;
+let imageCount = document.images.length;
+let userurl = localStorage.getItem("userInput");
 
 for (let i = 0; i < imageCount; i++) {
     const image = images[i];
     const newImage = document.createElement("img");
-    newImage.src = "https://raw.githubusercontent.com/DanceItBreakIt/image_replacer/refs/heads/main/icon48.png";
+    newImage.src = userurl;
     newImage.alt = "replacement image";
     image.parentNode.replaceChild(newImage, image);
 }
